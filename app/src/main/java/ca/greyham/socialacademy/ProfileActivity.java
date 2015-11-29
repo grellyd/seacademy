e08c7bb7bcb4bcaa0117a0f4d9518d968d3e0ae9
Lellow <graham.brown137@gmail.com>
2015-11-29 11:25:56 -0800

    Working profile page

--- app/src/main/java/ca/greyham/socialacademy/ProfileActivity.java
+++ app/src/main/java/ca/greyham/socialacademy/ProfileActivity.java
@@ -6,19 +6,21 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class ProfileActivity extends Activity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+
+public class ProfileActivity extends YouTubeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-//        FragmentManager fragmentManager = getFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//
-//        fragmentManager.beginTransaction();
-//        Fragment newProfile = ProfileFragment.newInstance("", "");
-//        fragmentTransaction.add(R.id.profileLinearLayout, newProfile, "PROFILE_ID");
-//        fragmentTransaction.commit();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentManager.beginTransaction();
+        ProfileFragment newProfile = ProfileFragment.newInstance("", "");
+        fragmentTransaction.add(R.id.profileLinearLayout, newProfile, "PROFILE_ID");
+        fragmentTransaction.commit();
     }
 }

779b1afe0afb09d86ab12610a60be7973e9977ee
Lellow <graham.brown137@gmail.com>
2015-11-28 16:06:25 -0800

    bare bones app

---
+++ app/src/main/java/ca/greyham/socialacademy/pitchscrollactivity.java
@@ -0,0 +1,14 @@
+package ca.greyham.socialacademy;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class pitchscrollactivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pitchscrollactivity);
+    }
+
+}
